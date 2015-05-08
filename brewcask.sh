ce1021d8f06fc841cbd879ef9b4157a0ac3e8200
Jason Tucker <jason@tucker.pro>
2015-05-08 10:01:02 -0700

    Organized all of the apps

--- brewcask.sh
+++ brewcask.sh
@@ -11,59 +11,59 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 #Get Brew Cask installed
 brew install caskroom/cask/brew-cask
 
-brew cask install rescuetime
-brew cask install skitch
+#Needed
+brew cask install google-chrome #Google's Web Browser
+brew cask install spotify #Music App
+brew cask install dropbox #Where "all the things" live
+brew cask install flip4mac #video codecs
 
-brew cask install window-tidy
-brew cask install viscosity
-
-brew cask install textexpander
-
-brew cask install vagrant
-brew cask install vagrant-manager
-brew cask install virtualbox
-brew cask install GrandPerspective
-brew cask install alfred
-brew cask install airserver
-brew cask install dropbox
-brew cask install dropbox
-brew cask install angry-ip-scanner
-brew cask install slack
-brew cask install vlc
-brew cask install bartender
-brew cask install skitch
-brew cask install evernote
-brew cask install google-chrome
-
-brew cask install spotify
-brew cask install sublime-text
-brew cask install appcleaner
-brew cask install archey
-brew cask install bartender
-brew cask install coconutbattery
-brew cask install codekit
-brew cask install commandq
-brew cask install dropbox
-brew cask install electric-sheep 
-brew cask install firefox
-brew cask install flash
-brew cask install flavours
-brew cask install flip4mac
-brew cask install fluid
-brew cask install flux
-brew cask install gfxcardstatus
-brew cask install github
-brew cask install google-chrome
-brew cask install google-drive
-brew cask install iterm2
-brew cask install jumpcut
-brew cask install nottingham
-brew cask install paparazzi
-brew cask install pupil
-brew cask install secrets
-brew cask install sequel-pro
+#Utils
+brew cask install rescuetime #track your time online
+brew cask install skitch #Screencapture
+brew cask install authy #Google Authenticator replacement
+brew cask install caffeine #Keep your computer awake
+brew cask install vlc #Play any video
+brew cask install appcleaner #removes apps
+brew cask install bartender #Cleans up your menu bar
+brew cask install commandq #Stops you from quitting apps
+brew cask install coconutbattery #Battery Manager
+brew cask install fluid #App Window tool
+brew cask install flux #Eye saver
+brew cask install gfxcardstatus #Graphics card utility
+brew cask install pupil  #App for screen resoultions in menu bar
+brew cask install secrets #lots of cool changes to the OS
+brew cask install thisservice #Services app
 brew cask install sequential
-brew cask install shortcat
-brew cask install sublime-text
-brew cask install superduper
-brew cask install thisservice
+
+#Productivity
+brew cask install textexpander #Text snippet utility
+brew cask install busycal #calendar replacement
+brew cask install alfred #App launcher - cmd space all the things
+brew cask install window-tidy #Clean up your windows
+brew cask install evernote #Where I store everything
+
+
+#Coding
+brew cask install coda #My dev enviroment
+brew cask install vagrant #Web server enviroment
+brew cask install vagrant-manager #managed the above
+brew cask install virtualbox #hosts the vagrants
+brew cask install sublime-text #Other coding enviroment
+brew cask install codekit #awesome web utility
+brew cask install firefox #browser I use for testing
+brew cask install flash #flash for Firefox
+brew cask install github #commit all the things
+brew cask install paparazzi #grabs images from websites
+brew cask install sequel-pro #SQL server manager
+
+#Administrator
+brew cask install viscosity #my vpn software
+brew cask install GrandPerspective #awesome disk viewer
+brew cask install angry-ip-scanner #scan the network
+brew cask install airserver #make your mac into an apple tv
+brew cask install iterm2 #terminal replacement
+brew cask install superduper #backup software
+
+#Social
+brew cask install colloquy #IRC client
+brew cask install slack #slack client
